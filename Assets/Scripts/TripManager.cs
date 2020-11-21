ee6269dc2c1eac1b87e91786bc5b09a4dd39ffa4
LiamReilly <liamreilly22@yahoo.com>
2020-11-21 17:24:18 -0500

    character selections continue to trip scene

---
+++ Assets/Scripts/TripManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TripManager : MonoBehaviour
+{
+    public GameObject player1;
+    public GameObject player2;
+
+    private GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        Sprite newSprite = gameManager.getPlayer1().GetComponent<SpriteRenderer>().sprite;
+        player1.GetComponent<SpriteRenderer>().sprite = newSprite;
+        Sprite newSprite2 = gameManager.getPlayer2().GetComponent<SpriteRenderer>().sprite;
+        player2.GetComponent<SpriteRenderer>().sprite = newSprite2;
+    }
+
+}
